59fd2e4d15be88843de3a42beb06d0d979f8ac39
Marcos Vaz <marcosvaz.me@gmail.com>
2019-05-22 21:12:37 -0300

    Added login and home screens

---
+++ app/src/main/java/br/com/marcosvaz/login/HomeActivity.kt
@@ -0,0 +1,22 @@
+package br.com.marcosvaz.login
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_home.*
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        val nome = intent.getStringExtra("nome");
+
+        txv_hello.text = "Olá $nome!";
+
+        btn_logout.setOnClickListener {
+            finish();
+        }
+    }
+}
--- app/src/main/java/br/com/marcosvaz/login/MainActivity.kt
+++ app/src/main/java/br/com/marcosvaz/login/MainActivity.kt
@@ -1,12 +1,28 @@
 package br.com.marcosvaz.login
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_verificar.setOnClickListener {
+            if(edt_nome.text.toString() == "marcos" && edt_senha.text.toString() == "12345"){
+                val intent = Intent(this, HomeActivity::class.java);
+                val nome = edt_nome.editableText.toString();
+                intent.putExtra("nome", nome);
+                startActivity(intent);
+            } else {
+                Toast.makeText(
+                    this, "Usuário e senha inválidos", Toast.LENGTH_LONG
+                ).show();
+            }
+        }
     }
 }
